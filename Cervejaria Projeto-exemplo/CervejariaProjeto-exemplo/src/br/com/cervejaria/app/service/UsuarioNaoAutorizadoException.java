4ac19eb71c98ef0dd92c0c66293de6108b28e674
Johny Klein <johnyklein@hotmail.com>
2018-08-24 19:49:52 -0300

    Nova class

--- Cervejaria Projeto-exemplo/CervejariaProjeto-exemplo/src/br/com/cervejaria/app/service/UsuarioNaoAutorizadoException.java
+++ Cervejaria Projeto-exemplo/CervejariaProjeto-exemplo/src/br/com/cervejaria/app/service/UsuarioNaoAutorizadoException.java
@@ -1,5 +1,7 @@
 package br.com.cervejaria.app.service;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.ws.WebFault;
 
 @WebFault(targetNamespace="http://service.app.cervejaria.com.br/excessoes/",
@@ -14,4 +16,17 @@ public class UsuarioNaoAutorizadoException extends RuntimeException {
 		super(mensagem);
 	}
 	
+	@XmlAccessorType(XmlAccessType.FIELD)
+	public class UsuarioFaultInfo {
+		public String message;
+		
+		public UsuarioFaultInfo(){
+			
+		}
+		
+		public UsuarioFaultInfo(String message){
+			this.message = message;
+		}
+		
+	}
 }
