f46fbb23c10b3ae812eaab9c2d1db825f3b60815
Johny Klein <johnyklein@hotmail.com>
2018-08-30 20:10:56 -0300

    Adicionado class para melhorar exception

--- Cervejaria Projeto-pratica/CervejariaProjeto-pratica/src/br/com/cervejaria/app/service/UsuarioNaoAutorizadoException.java
+++ Cervejaria Projeto-pratica/CervejariaProjeto-pratica/src/br/com/cervejaria/app/service/UsuarioNaoAutorizadoException.java
@@ -1,5 +1,34 @@
 package br.com.cervejaria.app.service;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.ws.WebFault;
+
+@WebFault(targetNamespace="http://service.app.cervejaria.com.br/excessoes/", name="UsuarioNaoAutorizadoException")
 public class UsuarioNaoAutorizadoException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+
+	public UsuarioNaoAutorizadoException() {
+		super();
+	}
+
+	public UsuarioNaoAutorizadoException(String mensagem) {
+		super(mensagem);
+	}
+	
+	@XmlAccessorType(XmlAccessType.FIELD)
+	public class UsuarioFaultInfo {
+		public String message;
+		
+		public UsuarioFaultInfo(){
+			
+		}
+		
+		public UsuarioFaultInfo(String message){
+			this.message = message;
+		}
+		
+	}
 	
 }
